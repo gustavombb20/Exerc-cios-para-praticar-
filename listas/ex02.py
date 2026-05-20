aa71db32986a5d80e1fd32f0dc4b24019f98d5b6
Gustavo <gustavombb20l@gmail.com>
2026-05-20 11:39:30 -0300

    Exercícios para para praticar

---
+++ listas/ex02.py
@@ -0,0 +1,6 @@
+#CRIE UMA LISTA DE 4 NUMEROS E MOSTRE TODOS USANDO FOR
+
+numeros = [10,20,30,40]
+
+for num in numeros:
+    print(num)
---
+++ listas/ex03.py
@@ -0,0 +1,9 @@
+#PEÇA 5 NOMES AO USUARIO E GUARDE EM UMA LISTA
+
+nomes = []
+
+for i in range(5):
+    nome = input('Digite 5 nomes:')
+    nomes.append(nome)
+
+print(nomes)
